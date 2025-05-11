02d07d6fe3bcae9889f7ca65668b30a4aebdb4cd
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2025-05-11 14:54:16 +0600

    This Is My First Commit

---
+++ app/src/main/java/com/asif/noteslagbe/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.asif.noteslagbe;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        }, 4000);
+
+
+    }
+}
